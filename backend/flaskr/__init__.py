e69e62eae3f81c98fd41a5a8e120ebece152c6f1
Widad Alkibsi <widad.alk@gmail.com>
2020-06-10 22:36:53 +0200

    ínitial message

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -38,19 +38,6 @@ def create_app(test_config=None):
     })
 
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
-
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
-
   @app.route('/questions' , methods=['GET'])
   def retrieve_questions():
     page = request.args.get('page', 1, type=int)
@@ -101,16 +88,6 @@ def create_app(test_config=None):
     except: 
       abort(422) 
 
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
-
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
   @app.route('/questions', methods=['POST'])
   def create_question():
     body = request.get_json()
@@ -150,14 +127,7 @@ def create_app(test_config=None):
          abort(422) # Unprocessable Entity
 
 
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
-
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
+  
   @app.route('/categories/<int:category_id>/questions' , methods=['GET'])
   def get_questions_basedon_category(category_id):
 
@@ -178,22 +148,12 @@ def create_app(test_config=None):
     return jsonify({
         'success' : True, 
         'questions': formatted_questions[start:end],
-        'total questions in category': len(formatted_questions),
-        'Category': formatted_category
+        'total questions': len(questions),
+        'current_category': category_id
         # 'categories': formatted_categories
     })
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-    '''
+  
   @app.route('/quizzes', methods=['POST'])
   def play_quiz():
     body = request.get_json()
@@ -228,17 +188,11 @@ def create_app(test_config=None):
       
                     })
 
-
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
-  # @app.errorhandler(400)
-  # def bad_request(error):
-  #       return jsonify({'success': False,
-  #                       'error': 400,
-  #                       'message': 'Bad request'}), 400
+  @app.errorhandler(400)
+  def bad_request(error):
+        return jsonify({'success': False,
+                        'error': 400,
+                        'message': 'Bad request'}), 400
   @app.errorhandler(404)
   def not_Found(error):
         return jsonify({'success': False,
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -29,10 +29,7 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    
     def test_qet_categories(self):
         res = self.client().get('/categories')
         data = json.loads(res.data)
